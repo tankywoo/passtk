5a1ce46c3e6f3c9892c6d668869c1f0e645c7761
Tanky Woo <wtq1990@gmail.com>
2013-01-09 03:01:37 +0800

    test passtk

--- passtk.py
+++ passtk.py
@@ -15,6 +15,7 @@
 
 import argparse
 import string
+import random
 
 digit = string.digits
 lower = string.lowercase
@@ -22,12 +23,20 @@ upper = string.uppercase
 punctuation_1 = '~!@#$%^&*'
 punctuation_2 = string.punctuation
 
+str_1 = lower
+str_2 = lower + upper
+str_3 = lower + upper + digit
+str_4 = lower + upper + digit + punctuation_1
+str_5 = lower + upper + digit + punctuation_2
+
 
 def generate():
-	pass
+	print ''.join(random.sample(str_5, 8))
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='')
 	parser.add_argument('-l', '--level', dest='level', default=3,help='')
 	args = parser.parse_args()
+
+	generate()
