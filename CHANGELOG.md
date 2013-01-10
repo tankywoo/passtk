bf1baa5601e8fe7ab76003a14b391b9bdf93cd20
Tanky Woo <wtq1990@gmail.com>
2013-01-10 17:23:51 +0800

    fix some bug by using random.sample

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+2013/01/10
+The random.sample will chooses a *set of unique* letters.
+So if the number is greater than the set's length, will throw an error:
+ValueError: sample larger than population
--- passtk.py
+++ passtk.py
@@ -5,7 +5,7 @@
 
 # Argument:
 # -l/--level : 1-3, default is 2
-# -n/--length : the length of the password # TODO
+# -n/--length : the length of the password
 
 # level 1 : a-zA-Z
 # level 2 : a-zA-Z0-9	(default)
@@ -21,6 +21,12 @@ UPPER = string.ascii_uppercase
 LETTER = string.ascii_letters
 PUNCTUATION = string.punctuation
 
+def _filter(level):
+	if level < 1:
+		level = 1
+	if level > 3:
+		level = 3
+	return level
 
 def shuffle(pwd):
 	_pwd = list(pwd)
@@ -28,47 +34,43 @@ def shuffle(pwd):
 	return ''.join(_pwd)
 
 
-def level_one(length):
-	lower_num = random.randint(1,length-1)
-	lower_str = str().join(random.sample(LOWER, lower_num))
-	upper_num = length - lower_num
-	upper_str = str().join(random.sample(UPPER, upper_num))
+def choice_n(seq, n):
+	# If use random.sample, the return is a set
+	r_lst = list()
+	for i in xrange(n):
+		r_lst.append(random.choice(seq))
+	return str().join(r_lst)
+
+
+def gen_pass(level, length):
+	lower_num = random.randint(1, length-level)
+	lower_str = choice_n(LOWER, lower_num)
+
+	upper_num = random.randint(1, length - lower_num-(level-1))
+	upper_str = choice_n(UPPER, upper_num)
+
 	pass_str = lower_str + upper_str
-	return pass_str
 
+	if level > 1:
+		if level == 2:
+			digit_num = length - lower_num - upper_num
+		else:
+			digit_num = random.randint(1, length - lower_num - upper_num - (level-2))
+		digit_str = choice_n(DIGIT, digit_num)
+		pass_str += digit_str
 
-def level_two(length):
-	lower_num = random.randint(1,length-2)
-	lower_str = str().join(random.sample(LOWER, lower_num))
-	upper_num = random.randint(1,length - lower_num-1)
-	upper_str = str().join(random.sample(UPPER, upper_num))
-	digit_num = length - lower_num - upper_num
-	digit_str = str().join(random.sample(DIGIT, digit_num))
-	pass_str = lower_str + upper_str + digit_str
-	return pass_str
-
-
-def level_three(length):
-	lower_num = random.randint(1,length-3)
-	lower_str = str().join(random.sample(LOWER, lower_num))
-	upper_num = random.randint(1,length - lower_num-2)
-	upper_str = str().join(random.sample(UPPER, upper_num))
-	digit_num = random.randint(1,length - lower_num - upper_num - 1)
-	digit_str = str().join(random.sample(DIGIT, digit_num))
-	punc_num = random.randint(1,length - lower_num - upper_num - digit_num)
-	punc_str = str().join(random.sample(PUNCTUATION, punc_num))
-	pass_str = lower_str + upper_str + digit_str + punc_str
-	return pass_str
-
+	if level == 3:
+		punc_num = length - lower_num - upper_num - digit_num
+		punc_str = choice_n(PUNCTUATION, punc_num)
+		pass_str += punc_str
 	
+	return pass_str
+	
+
 def generate(level=2, length=8):
+	# Main Function
 	random.seed()
-	if level == 1:
-		pass_str = level_one(length)
-	elif level == 2:
-		pass_str = level_two(length)
-	else:
-		pass_str = level_three(length)
+	pass_str = gen_pass(level, length)
 	res = shuffle(pass_str)
 	print res
 
@@ -81,6 +83,6 @@ if __name__ == '__main__':
 			help='The length of the password')
 	args = parser.parse_args()
 	
-	level = args.level
+	level = _filter(args.level)
 	length = args.length
 	generate(level,length)
