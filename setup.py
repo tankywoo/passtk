0fe277c36ba1bb190da43bef0af806c89a350128
Tanky Woo <wtq1990@gmail.com>
2013-12-05 00:21:12 +0800

    packaging

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup, find_packages
+
+
+entry_points = {
+    "console_scripts": [
+        "passtk = passtk.passtk:main",
+    ]
+}
+
+requires = []
+if sys.version_info < (2,7):
+    requires.append('argparse')
+
+setup(
+    name = "passtk",
+    version = "0.1",
+    url = "https://github.com/tankywoo/passtk",
+    author = "Tanky Woo",
+    author_email = "me@tankywoo.com",
+    description = "A tool to generate random password.",
+    license = "MIT License",
+    packages = find_packages(),
+    install_requires = requires,
+    entry_points = entry_points,
+)
